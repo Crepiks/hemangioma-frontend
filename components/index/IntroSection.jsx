2ed4518f14fc5d824e87963c86ee427bae278220
Crepiks <sayazhan.onlassyn@mail.ru>
2019-12-25 20:47:47 +0600

    Make Intro section responsive

--- components/index/IntroSection.jsx
+++ components/index/IntroSection.jsx
@@ -12,16 +12,51 @@ const Wrapper = styled.section`
     align-items: center;
     color: #924E4E;
     font-family: 'Roboto Slab', sans-serif;
+
+    @media all and (max-width: 800px) {
+        flex-direction: column-reverse;
+        justify-content: center;
+    }
 `
 
 const Column = styled.div`
     width: 45%;
     box-sizing: border-box;
+
+    @media all and (max-width: 900px) {
+        width: 40%;
+    }
+
+    @media all and (max-width: 800px) {
+        width: 80%;
+    }
+
+    @media all and (max-width: 500px) {
+        width: 90%;
+    }
 `
 
 const Title = styled.h1`
     font-size: 38px;
     line-height: 1;
+
+    @media all and (max-width: 1200px) {
+        font-size: 30px;
+    }
+
+    @media all and (max-width: 900px) {
+        font-size: 26px;
+    }
+
+    @media all and (max-width: 800px) {
+        margin-top: 30px;
+        font-size: 30px;
+        text-align: center;
+    }
+
+    @media all and (max-width: 600px) {
+        font-size: 22px;
+    }
 `
 
 const Subtitle = styled.h2`
@@ -29,15 +64,40 @@ const Subtitle = styled.h2`
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 20px;
+
+    @media all and (max-width: 1200px) {
+        font-size: 16px;
+    }
+
+    @media all and (max-width: 800px) {
+        font-size: 16px;
+        text-align: center;
+    }
 `
 
 const StyledButton = styled(Button)`
     margin-top: 30px;
     padding: 15px 30px;
+
+    @media all and (max-width: 1200px) {
+        margin: 20px auto 0;
+    }
 `
 
 const Image = styled.img`
+    width: 50%;
 
+    @media all and (max-width: 900px) {
+        width: 60%;
+    }
+
+    @media all and (max-width: 800px) {
+        width: 80%;
+    }
+
+    @media all and (max-width: 500px) {
+        width: 100%;
+    }
 `
 
 const Intro = ({ className }) => (
@@ -47,9 +107,7 @@ const Intro = ({ className }) => (
             <Subtitle>Hemangioma.kz - ведущий центр лечения гемангиомы и других кровеных заболеваний в Казахстане</Subtitle>
             <StyledButton>Наши услуги</StyledButton>
         </Column>
-        <Column>
-            <Image src={doctor_with_client} alt="Доктор с клиентом" />
-        </Column>
+        <Image src={doctor_with_client} alt="Доктор с клиентом" />
     </Wrapper>
 )
 
