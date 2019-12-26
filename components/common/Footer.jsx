c9ff4091d7824db6f337976b5088a86a3081f5cc
Crepiks <sayazhan.onlassyn@mail.ru>
2019-12-26 14:45:16 +0600

    Add Footer's content

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -5,18 +5,57 @@ import Logo from './Logo'
 const Wrapper = styled.footer`
     height: 350px;
     padding: 70px 10vw;
+    display: flex;
+    justify-content: space-between;
     background-color: #924E4E;
     box-sizing: border-box;
     color: #ffffff;
+`
+
+const Column = styled.div`
+    display: flex;
+    flex-direction: column;
 `
 
 const StyledLogo = styled(Logo)`
     color: #ffffff;
 `
 
+const Title = styled.h4`
+    margin-bottom: 15px;
+    font-family: 'Roboto Slab', serif;
+    font-size: 24px;
+    font-weight: 400;
+`
+
+const Link = styled.a`
+    margin-bottom: 5px;
+    font-family: 'Roboto', sans-serif;
+    cursor: pointer;
+`
+
+const Text = styled.span`
+    margin-bottom: 5px;
+    font-family: 'Roboto', sans-serif;
+`
+
 const Footer = ({ className }) => (
     <Wrapper className={className}>
-        <StyledLogo theme="light" />
+        <Column>
+            <StyledLogo theme="light" />
+        </Column>
+        <Column>
+            <Title>Ссылки</Title>
+            <Link href="#">Главная</Link>
+            <Link href="#">О нас</Link>
+            <Link href="#">Галерея</Link>
+            <Link href="#">Конакты</Link>
+        </Column>
+        <Column>
+            <Title>Контакты</Title>
+            <Text>Адрес: <Link>г.Алматы, Абая 160, 6</Link></Text>
+            <Text>Телефон: <Link>+7 707 507 75 96</Link></Text>
+        </Column>
     </Wrapper>
 )
 
