2121409ce4db5b06c3731a17571dd6dd297d8bf9
Crepiks <sayazhan.onlassyn@mail.ru>
2019-12-26 15:25:01 +0600

    Create contact page with basic skeleton

---
+++ pages/contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from '../components/common/Head'
+import Header from '../components/common/Header'
+import Footer from '../components/common/Footer'
+import '../assets/style/reset.css'
+import '../assets/style/main.css'
+
+const ContactPage = () => (
+    <React.Fragment>
+        <Head />
+        <Header />
+        <Footer />
+    </React.Fragment>
+)
+
+export default ContactPage
